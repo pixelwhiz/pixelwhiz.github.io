421fbabbc95d681ffbbef587108c5206d945d4ea
pixelwhiz <daffa01futn@gmail.com>
2024-12-09 12:45:45 +0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ const App: React.FC = () => {
 
       const status = response.data.data.activities[0].name;
 
-      if (status === "Spotify") {
+      if (status === "Spotify" || "YouTube Music") {
         setActivity("Listening to");
       } else {
         setActivity("Playing");
@@ -284,7 +284,7 @@ const App: React.FC = () => {
                               {playing_status ? (
                                   <span className={"flex-wrap font-medium text-wrap"}>{activity} <span className={"me-0.5"}>{playing_status}</span> <svg width="26" height="26" viewBox="0 0 16 16" fill="currentColor" className="inline-flex -mt-0.5"><path d="M6,7 L2,7 L2,6 L6,6 L6,7 Z M8,5 L2,5 L2,4 L8,4 L8,5 Z M8,3 L2,3 L2,2 L8,2 L8,3 Z M8.88888889,0 L1.11111111,0 C0.494444444,0 0,0.494444444 0,1.11111111 L0,8.88888889 C0,9.50253861 0.497461389,10 1.11111111,10 L8.88888889,10 C9.50253861,10 10,9.50253861 10,8.88888889 L10,1.11111111 C10,0.494444444 9.5,0 8.88888889,0 Z" transform="translate(0 3)"></path></svg></span>
                               ) : (
-                                  <span className={"flex-wrap font-medium text-wrap"}>{status}</span>
+                                  <span className={"flex-wrap font-medium text-wrap"}>No activity is going on</span>
                               )}
                             </label>
                         )}
